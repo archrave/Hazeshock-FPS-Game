1ecfa714c557c7899307c2d903ef9f032a56c20d
Divyansh Joshi <57811359+archrave@users.noreply.github.com>
2021-04-02 17:54:29 +0530

    Added two more npc difficulties and a new head bisection
    Created two more enemy difficulties, which spawn according to the score. Also Created a  new enemy head bisection to spawn wherever a enemy is killed with a headshot

--- HAZESHOCK/Assets/Scripts/spawnnpc.cs
+++ HAZESHOCK/Assets/Scripts/spawnnpc.cs
@@ -4,26 +4,50 @@ using UnityEngine;
 
 public class spawnnpc : MonoBehaviour
 {
-    public GameObject npc;
-    public float respawntime = 5f;
+    public GameObject npc1;
+    public GameObject npc2;
+    public GameObject npc3;
+    public float score_ref;
+    public float respawntime1 = 5f;
+    public float respawntime2 = 3f;
+    public float respawntime3 = 2f;
     void Start()
     {
         StartCoroutine(enemywave());
+        
     }
-    private void Spawn()
+    private void Spawn(GameObject npcname)
     {
-        GameObject a = Instantiate(npc) as GameObject;
+        GameObject a = Instantiate(npcname) as GameObject;
         a.transform.position = new Vector3(Random.Range(-45f, 45f), 2, Random.Range(0, 145f));
     }
     IEnumerator enemywave()
     {
-        while (true) { 
-            yield return new WaitForSeconds(respawntime);
-            Spawn();
+        while (true) {
+            if (score_ref < 5f)
+            {
+                yield return new WaitForSeconds(respawntime1);
+                Spawn(npc1);
+            }
+            else if (score_ref >= 5f && score_ref < 10f)
+            {
+               
+                yield return new WaitForSeconds(respawntime2);
+                Spawn(npc2);
+                Spawn(npc1);
+            }
+            else if (score_ref >= 10f)
+            {               
+                yield return new WaitForSeconds(respawntime3);
+                Spawn(npc3);
+                Spawn(npc2);
+            }
+
         }
     }
     void Update()
     {
-        
+        score_ref = FindObjectOfType<updatescore>().score_count;
     }
+  
 }
